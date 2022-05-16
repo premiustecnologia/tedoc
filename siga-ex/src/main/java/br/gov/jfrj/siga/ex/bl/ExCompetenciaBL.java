5638142ddf3311719d80e26469e2bb6aae17826e
Wendell Clive Santos de Lira <wendell@codata.pb.gov.br>
2022-05-16 18:50:43 -0300

    Ocultar botao cancelar

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExCompetenciaBL.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExCompetenciaBL.java
@@ -889,15 +889,11 @@ public class ExCompetenciaBL extends CpCompetenciaBL {
 					CpTipoConfiguracao.TIPO_CONFIG_MOVIMENTAR, null, null, null, null, null, null))
 				return false;				
 		} else {
-			if(!mob.doc().isDocFilhoJuntadoAoPai() && mob.doc().getSubscritor().equivale(titular)) {
+			if(!mob.doc().getSubscritor().equivale(titular)) {
+				return false;
 			} else {
-				if(mob.doc().getSubscritor() == null || !mob.doc().getSubscritor().equivale(titular)) {
+				if (podeSerTransferido(mob)) {  
 					return false;
-				} else {
-					if (podeSerTransferido(mob)) { } 
-					else {
-						return false;
-					}
 				}
 			}
 		}
