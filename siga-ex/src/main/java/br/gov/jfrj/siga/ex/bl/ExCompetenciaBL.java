90885e6b59b78b14b47cb54f2a5e58fd0f2ab002
caiosantos-codata <88742191+caiosantos-codata@users.noreply.github.com>
2023-01-23 13:08:28 -0300

    [BUGFIX] NPE para casos onde o subscritor é nulo (#323)

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExCompetenciaBL.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExCompetenciaBL.java
@@ -889,7 +889,7 @@ public class ExCompetenciaBL extends CpCompetenciaBL {
 					CpTipoConfiguracao.TIPO_CONFIG_MOVIMENTAR, null, null, null, null, null, null))
 				return false;				
 		} else {
-			if(!mob.doc().getSubscritor().equivale(titular)) {
+			if(mob.doc().getSubscritor() != null && !mob.doc().getSubscritor().equivale(titular)) {
 				return false;
 			} 
 			if (!podeSerTransferido(mob) && mob.doc().isDocFilhoJuntadoAoPai()) {  
